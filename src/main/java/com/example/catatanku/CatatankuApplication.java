a3730854e6e63278eeb67b23f1b58e0dd79db149
alviansyahexza <alviansyahexza@gmail.com>
2024-08-08 09:11:33 +0700

    download starter

---
+++ src/main/java/com/example/catatanku/CatatankuApplication.java
@@ -0,0 +1,13 @@
+package com.example.catatanku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatatankuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatatankuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/catatanku/CatatankuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catatanku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatatankuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
